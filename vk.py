d8017bea003f747c8c22641b3937d343285178d3
Олег Призов <oleg12345prisov@gmail.com>
2026-04-26 23:15:13 +0300

    update

--- vk.py
+++
@@ -1,6 +0,0 @@
-from vk_integration.services import send_vk_message
-from users.models import User
-
-u = User.objects.get(username='oleg')
-
-send_vk_message(u.vk_user_id, 'Тестовое уведомление из Django')
